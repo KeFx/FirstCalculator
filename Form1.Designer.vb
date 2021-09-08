d02215639cfbef767e7035d97e22043c428c2cc6
Ke Hu <keywhobp@gmail.com>
2021-09-08 20:46:31 +1200

    Visual improvement of form

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -30,36 +30,40 @@ Partial Class Form1
         Me.result = New System.Windows.Forms.TextBox()
         Me.Multiply = New System.Windows.Forms.Button()
         Me.Divide = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Plus
         '
-        Me.Plus.Location = New System.Drawing.Point(125, 241)
+        Me.Plus.Location = New System.Drawing.Point(94, 259)
         Me.Plus.Name = "Plus"
-        Me.Plus.Size = New System.Drawing.Size(112, 34)
+        Me.Plus.Size = New System.Drawing.Size(39, 34)
         Me.Plus.TabIndex = 0
         Me.Plus.Text = "+"
         Me.Plus.UseVisualStyleBackColor = True
         '
         'Minus
         '
-        Me.Minus.Location = New System.Drawing.Point(125, 354)
+        Me.Minus.Location = New System.Drawing.Point(94, 328)
         Me.Minus.Name = "Minus"
-        Me.Minus.Size = New System.Drawing.Size(112, 34)
+        Me.Minus.Size = New System.Drawing.Size(39, 34)
         Me.Minus.TabIndex = 1
         Me.Minus.Text = "-"
         Me.Minus.UseVisualStyleBackColor = True
         '
         'First
         '
-        Me.First.Location = New System.Drawing.Point(106, 75)
+        Me.First.Location = New System.Drawing.Point(94, 145)
         Me.First.Name = "First"
         Me.First.Size = New System.Drawing.Size(150, 31)
         Me.First.TabIndex = 2
         '
         'Second
         '
-        Me.Second.Location = New System.Drawing.Point(340, 75)
+        Me.Second.Location = New System.Drawing.Point(280, 145)
         Me.Second.Name = "Second"
         Me.Second.Size = New System.Drawing.Size(150, 31)
         Me.Second.TabIndex = 3
@@ -67,7 +71,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(562, 78)
+        Me.Label1.Location = New System.Drawing.Point(466, 151)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(24, 25)
         Me.Label1.TabIndex = 4
@@ -75,34 +79,74 @@ Partial Class Form1
         '
         'result
         '
-        Me.result.Location = New System.Drawing.Point(656, 75)
+        Me.result.Location = New System.Drawing.Point(521, 145)
         Me.result.Name = "result"
         Me.result.Size = New System.Drawing.Size(150, 31)
         Me.result.TabIndex = 5
         '
         'Multiply
         '
-        Me.Multiply.Location = New System.Drawing.Point(340, 241)
+        Me.Multiply.Location = New System.Drawing.Point(169, 259)
         Me.Multiply.Name = "Multiply"
-        Me.Multiply.Size = New System.Drawing.Size(112, 34)
+        Me.Multiply.Size = New System.Drawing.Size(39, 34)
         Me.Multiply.TabIndex = 6
         Me.Multiply.Text = "*"
         Me.Multiply.UseVisualStyleBackColor = True
         '
         'Divide
         '
-        Me.Divide.Location = New System.Drawing.Point(340, 354)
+        Me.Divide.Location = New System.Drawing.Point(169, 328)
         Me.Divide.Name = "Divide"
-        Me.Divide.Size = New System.Drawing.Size(112, 34)
+        Me.Divide.Size = New System.Drawing.Size(39, 34)
         Me.Divide.TabIndex = 7
         Me.Divide.Text = "/"
         Me.Divide.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(94, 92)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(106, 25)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "1st Number"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(280, 92)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(113, 25)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "2nd Number"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(521, 92)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(69, 25)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Output"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(94, 204)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(104, 25)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Operations:"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(992, 477)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Divide)
         Me.Controls.Add(Me.Multiply)
         Me.Controls.Add(Me.result)
@@ -126,4 +170,8 @@ Partial Class Form1
     Friend WithEvents result As TextBox
     Friend WithEvents Multiply As Button
     Friend WithEvents Divide As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -14,4 +14,8 @@
     Private Sub Divide_Click(sender As Object, e As EventArgs) Handles Divide.Click
         result.Text = Single.Parse(First.Text) / Single.Parse(Second.Text)
     End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
 End Class

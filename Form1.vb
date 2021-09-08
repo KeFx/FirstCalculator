87b67d432a874cfb867173a8f1338f83e1726a49
Ke Hu <keywhobp@gmail.com>
2021-09-08 20:41:37 +1200

    Init Calculator1 program repo

--- Form1.vb
+++ Form1.vb
@@ -1,13 +1,17 @@
 ﻿Public Class Form1
     Private Sub Plus_Click(sender As Object, e As EventArgs) Handles Plus.Click
-        result.Text = Integer.Parse(First.Text) + Integer.Parse(Second.Text)
+        result.Text = Single.Parse(First.Text) + Single.Parse(Second.Text)
     End Sub
 
     Private Sub Minus_Click(sender As Object, e As EventArgs) Handles Minus.Click
-        result.Text = Integer.Parse(First.Text) - Integer.Parse(Second.Text)
+        result.Text = Single.Parse(First.Text) - Single.Parse(Second.Text)
     End Sub
 
     Private Sub Multiply_Click(sender As Object, e As EventArgs) Handles Multiply.Click
+        result.Text = Single.Parse(First.Text) * Single.Parse(Second.Text)
+    End Sub
 
+    Private Sub Divide_Click(sender As Object, e As EventArgs) Handles Divide.Click
+        result.Text = Single.Parse(First.Text) / Single.Parse(Second.Text)
     End Sub
 End Class
